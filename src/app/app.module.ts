86c5005545d13601afa97f6365228114610bdded
raphaeldobu <jose.bu@ccc.ufcg.edu.br>
2018-07-01 12:21:29 -0300

    adição do component combustivel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,15 @@ import { PostoService } from './service/posto.service';
 import { AuthService } from './service/auth.service';
 import {TokenStorage} from './service/token.storage';
 import { Interceptor } from './service/interceptor';
+import { CombustivelComponent } from './component/combustivel/combustivel.component';
 
 const appRoutes:Routes = [
   { path: "add-posto", component: AddPostoComponent},
   { path: "add-cliente", component: AddClienteComponent},
   { path: "login", component: LoginComponent},
   { path: "lista-clientes", component: ClienteComponent},
-  { path: "lista-postos", component: PostoComponent}
+  { path: "lista-postos", component: PostoComponent},
+  { path: "add-combustivel", component: CombustivelComponent}
 ];
 @NgModule({
   declarations: [
@@ -32,7 +34,8 @@ const appRoutes:Routes = [
     PostoComponent,
     LoginComponent,
     AddClienteComponent,
-    AddPostoComponent
+    AddPostoComponent,
+    CombustivelComponent,
   ],
   imports: [
     BrowserModule,
