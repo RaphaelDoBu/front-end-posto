eb57c4b5e27bf2d4e27ad72762c76993d68ba2ff
raphaeldobu <jose.bu@ccc.ufcg.edu.br>
2018-08-02 22:13:08 -0300

    modificações

--- src/serves.js
+++ src/serves.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/angular'));
 
-app.get("*", function(req, res){
+app.get("/*", function(req, res){
 
     res.sendFile(path.join(__dirname + '/dist/angular/index.html'))
 });
